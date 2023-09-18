329752939286716dcf7452f57e74e4f1a89ad635
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-18 10:06:26 -0600

    fix: use lowercase

--- src/commands/pre-core.ts
+++ src/commands/pre-core.ts
@@ -2,6 +2,6 @@ import {Command} from '@oclif/command'
 
 export default class PreCore extends Command {
   async run(): Promise<void> {
-    this.log(`Hello I am a pre-core plugin from ${this.config.root}`)
+    this.log(`hello I am a pre-core plugin from ${this.config.root}`)
   }
 }
